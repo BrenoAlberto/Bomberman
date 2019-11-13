9f2a24b6f1638a0462304d4f7b12366b9e5c0417
breno.alberto <breno.alberto@avancoinfo.com.br>
2019-11-13 12:56:43 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,92 +1,108 @@
+class BombermanSimulacrum:
+    def __init__(self, num_rows, num_cols, n_seconds, grid):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.n_seconds = n_seconds
+        self.grid = grid
+        self.exploding_bombs_map = []
 
-from time import sleep
-import os
+    def start_simulation(self):
+        for second in range(self.n_seconds + 1):
+            for i in range(self.num_rows):
+                for j in range(self.num_cols):
 
-def in_range(grid, i, j):
-    num_rows, num_cols = len(grid), len(grid[0])
+                    if(self.grid[i][j] not in ['.', 'X']):
+                        self.increase_bomb_timer(i, j)
 
-    # print(i)
+                    if(second != 0 and second % 2 == 0 and self.grid[i][j] == '.'):
+                        self.plant_bomb(i, j)
 
-    if(i < 0 or j < 0 or i >= num_rows or j >= num_cols):
-	    return False
+            if (second != 1 and second % 2 == 1):
+                self.locate_and_detonate_bombs()
 
-    return True
+        self.print_grid()
 
-def detonate_bombs(grid, i, j):
-    if(not in_range(grid, i, j) or grid[i][j] == 'X'):
-        return 
+    def increase_bomb_timer(self, i, j):
+        if(self.grid[i][j] == 'O'):
+            self.grid[i][j] = '1'
 
+        elif(self.grid[i][j] == '1'):
+            self.grid[i][j] = '2'
 
-    print('===============================')
-    print('i', i)
-    print('j', j)
-    print('grid ixj', grid[i][j] )
-    # print('i' = )
-    print('===============================')
+        elif(self.grid[i][j] == '2'):
+            self.grid[i][j] = '3'
+            self.exploding_bombs_map.append([i, j])
 
-    if(grid[i][j] in ['O', '1'] ):
-        grid[i][j] = '.'
+    def plant_bomb(self, i, j):
+        self.grid[i][j] = 'O'
+
+    def in_range(self, i, j):
+        if(i < 0 or j < 0 or i >= self.num_rows or j >= self.num_cols or self.grid[i][j] in ['X']):
+            return False
+
+        return True
+
+    def locate_and_detonate_bombs(self):
+        if(len(self.exploding_bombs_map) > 0):
+            for bomb_location in self.exploding_bombs_map:
+                self.detonate_bomb(bomb_location[0], bomb_location[1])
+
+            self.exploding_bombs_map.clear()
+
+    def detonate_bomb(self, i, j):
+        self.grid[i][j] = '.'
+
+        r, c = i, j
+        while(r > 0):
+            r = r - 1
+            if(not self.in_range(r, c)):
+                break
+
+            self.grid[r][c] = '.'
+
+        r, c = i, j
+        while(r <= self.num_rows):
+            r = r + 1
+            if(not self.in_range(r, c)):
+                break
+
+            self.grid[r][c] = '.'
+
+        r, c = i, j
+        while(c > 0):
+            c = c - 1
+            if(not self.in_range(r, c)):
+                break
+
+            self.grid[r][c] = '.'
+
+        r, c = i, j
+        while(c <= self.num_rows):
+            c = c + 1
+            if(not self.in_range(r, c)):
+                break
+
+            self.grid[r][c] = '.'
         return
 
-    grid[i][j] = '.'
+    def print_grid(self):
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                if(self.grid[i][j] in ['1', '2']):
+                    self.grid[i][j] = 'O'
 
-    detonate_bomb(grid, i + 1, j)
-    detonate_bomb(grid, i - 1, j)
-    detonate_bomb(grid, i, j + 1)
-    detonate_bomb(grid, i, j - 1)
-    return     
-
-
-def increase_bomb_timer(grid, i, j):
-    if(grid[i][j] == 'O'):
-        grid[i][j] = '1'
-    elif(grid[i][j] == '1'):
-        grid[i][j] = '2'
-    else:
-        grid[i][j] = '3'
+        print('\n'.join(''.join(*zip(*row)) for row in self.grid))
 
 def get_input_grid():
-    # rows >= 1
-    # cols <= 200
-    # n >= 1 n <= 10**9
     num_rows, num_cols, n_seconds = input().split()
-
 
     grid = []
     for _ in range(int(num_rows)):
         grid.append(list(input()))
-    
+
     return int(num_rows), int(num_cols), int(n_seconds), grid
 
-# def check_grid():
 
-
-def start_bomberman():
-    num_rows, num_cols, n_seconds, grid = get_input_grid()
-    for second in range(n_seconds):
-        if(second != 0):
-            for i in range(num_rows):
-                for j in range(num_cols):
-                    if(grid[i][j] not in ['.', 'X']):
-                        increase_bomb_timer(grid, i, j)
-            detonate_bombs(grid, i, j)
-            # if (second % 2 == 0):
-                # plant bombs
-            # else if (second != 1 and second % 2 == 1):
-                # detonate bombs
-    print('\n'.join(''.join(*zip(*row)) for row in grid))
-
-start_bomberman()
-# os.system('cls' if os.name == 'nt' else 'clear')
-# sleep(2) 
-# print(grid)
-
-
-
-
-[ 
-    [0, 1, 2, 3],
-    [0, 1, 2, 3],
-    [0, 1, 2, 3],
-    [0, 1, 2, 3],
-]
+num_rows, num_cols, n_seconds, grid = get_input_grid()
+bomberman_simulacrum = BombermanSimulacrum(num_rows, num_cols, n_seconds, grid)
+bomberman_simulacrum.start_simulation()
